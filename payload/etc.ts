62975a676461dd85a8a81150deca0ddd5cd39002
namhyong <namhyong2@gmail.com>
2025-08-04 01:26:23 +0900

    feat: 20250804

--- payload/etc.ts
+++ payload/etc.ts
@@ -7,7 +7,17 @@ const etc: IEtc.Payload = {
     {
       title: 'KOTRA 공공데이터 활용 성공사례 공모전 우수상',
       subTitle: '중소기업 및 스타트업의 해외진출 활성화를 위한 RAG 기반 LLM AI 어시스턴트(KOTRA)',
-      startedAt: '2016-07',
+      startedAt: '2024-08',
+    },
+    {
+      title: '빅데이터 분석기사',
+      subTitle: '데이터 분석 자격증',
+      startedAt: '2025-07',
+    },
+    {
+      title: 'SQLD',
+      subTitle: '데이터베이스 자격증',
+      startedAt: '2022-09',
     },
     {
       title: '병장 만기 전역',
